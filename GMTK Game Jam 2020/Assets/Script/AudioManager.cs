59b6009be86046313148c6297df4921c7b85a307
Daniel Ribeiro <42726779+tsunas4@users.noreply.github.com>
2020-07-10 20:04:31 -0300

    push-teste
    audio manager

---
+++ GMTK Game Jam 2020/Assets/Script/AudioManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Array")]
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+        }
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void PlayByName(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s != null)
+        {
+            s.source.Play();
+        }
+        else
+        {
+            Debug.LogError("Não foi possível encontrar o som com o nome " + name);
+        }
+    }
+
+}
