d2f2cbeb0ea85735d62a3a9a18b99357437e4377
Daniel Ribeiro <42726779+tsunas4@users.noreply.github.com>
2020-07-11 23:56:00 -0300

    criacao do level manager

--- GMTK Game Jam 2020/Assets/Script/System/LevelManager.cs
+++ GMTK Game Jam 2020/Assets/Script/System/LevelManager.cs
@@ -1,18 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// Esse script é recriado em todo o level e gerencia o seu escopo
+/// </summary>
 public class LevelManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject endGameScreen;
+
+    int minutes = 0;
+    public int seconds = 0;
+    int hours = 0;
+    private void Start()
     {
-        
+    }
+    private void Update()
+    {
+
+        string hoursTxt = String.Format("00", hours);
+        string minutesTxt = String.Format("00", minutes);
+        string secondsTxt = String.Format("00", seconds);
+
+        GameObject.Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text = hoursTxt + ":" + minutesTxt + ":" + secondsTxt;
+
+        seconds += (int)Time.deltaTime;
+        if(seconds >= 60)
+        {
+            seconds = 0;
+            minutes++;
+        }
+
+        if(minutes > 60)
+        {
+            minutes = 0;
+            hours++;
+        }
+
+        if (minutes >= 10)
+        {
+            EndGame();
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndGame()
     {
-        
+        endGameScreen.SetActive(true);
     }
 }
